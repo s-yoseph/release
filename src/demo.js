9ff410e82c14ce56dcfb183818eff516498c39ac
s-yoseph <selamyoseph84@gmail.com>
2025-11-02 23:44:22 +0300

    6

--- src/demo.js
+++ src/demo.js
@@ -3,3 +3,4 @@
 //2 i will publish it the bug only
 //3 for enhancment
 //4 accedinatly said i will publish the bug instead of enhancmet so adding 4th commit 
+//6 for major i fixed the relese.yml so freash start bug
